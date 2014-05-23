58aa9e0d172f99aef396da7ffef1a0e2d53185af
Pompeu <itacir@hotmail.com>
2014-05-23 14:18:42 -0300

    humm
    100%

--- Aquecimento para a OBI URI 2014/Perguntas mais Frequentes/main.cpp
+++ Aquecimento para a OBI URI 2014/Perguntas mais Frequentes/main.cpp
@@ -1,22 +1,23 @@
 #include <cstdio>
 
-int main()
+int main( )
 {
     unsigned int n,k;
 
-    while(1){
-        scanf("%u %u",&n,&k);
-            if(n == 0) return 0;
+    while(scanf("%u %u",&n,&k), n+k > 0){
 
-        int p[1001] = {0},a,z=n,pr[n];
+        int p[1001]={0},a;
 
-           while(z--){
+           while(n--){
                 scanf("%d", &a);
                     p[a]++;
                 }
+            /*for(int i = 0;i < 101; i++)
+                printf("%i ",p[i]);*/
 
-            int maior = p[0] , pos = 0, nk=0;
-             for(int i = 0; i < n; i++){
+            int maior = 0 , pos = 0, nk=0;
+
+             for(int i = 0; i < 1001; i++){
                         if(maior <= p[i]){
                             maior = p[i];
                             pos = i;
